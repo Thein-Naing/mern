38081a669da0289054093f5944f3489065806aac
Thein Naing <theinnaing0908@gmail.com>
2023-05-19 11:53:26 +0800

    create get route  by app.get with two parameters , '/'=homepage and callback function with (rer, res) arguments and console to test

--- server.js
+++ server.js
@@ -4,9 +4,10 @@ import bodyParser from 'body-parser';
 // const express = require("express")
 const app = express()
 
-// app.get('/api', (req, res)=> {
-//   res.json({"users": ["userOne", "userTwo", "userThree"]})
-// })
+app.get('/', (req, res)=> {
+  console.log("test")
+  // res.json({"users": ["userOne", "userTwo", "userThree"]})
+})
 const PORT = 5000;
 
 app.use(bodyParser.json());
