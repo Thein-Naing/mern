3939d0829fa018ed8d1a0ff45efb880f44d724e0
Thein Naing <theinnaing0908@gmail.com>
2023-05-19 11:20:10 +0800

    create PORT = 5000, create app.use equal bodyParser.json() for post requests, create app.listen function for this requests with PORT argument and call back function argument} wrapping with template string

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 import express from 'express';
-import body-parser from 'body-parser'
+import bodyParser from 'body-parser';
 
-const express = require("express")
+// const express = require("express")
 const app = express()
 
-app.get('/api', (req, res)=> {
-  res.json({"users": ["userOne", "userTwo", "userThree"]})
-})
+// app.get('/api', (req, res)=> {
+//   res.json({"users": ["userOne", "userTwo", "userThree"]})
+// })
+const PORT = 5000;
+
+app.unsubscribe(bodyParser.json());
+
+app.listen(PORT, ()=> console.log(`Server running on port : http//localhost${PORT}`));
