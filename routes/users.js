c1c1b7287bc2652f3d72d1dcd8540dd5b8f775a0
Thein Naing <theinnaing0908@gmail.com>
2023-05-19 14:20:31 +0800

    npm i uuid, copy uuid path and key to users.js, create new const userWithID equal to {...user, id: uuidv4()}

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,6 @@
 import express from 'express';
+import { v4 as uuidv4 } from 'uuid';
+uuidv4(); // ⇨ '9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d'
 
 const router = express.Router();
 
@@ -26,6 +28,11 @@ router.get('/', (req, res)=> {
 
 router.post('/', (req, res)=> {
   const user = req.body;
+
+  // ⇨ '9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d
+
+  const userWithId = {...user, id: uuidv4( )}
+
   users.push(user)
 
   res.send(`User with the name ${ user.firstName } added to the DB!`)
