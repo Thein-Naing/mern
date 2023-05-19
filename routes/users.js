1f07d7c3945fed0fca15c32b5284802b102a5368
Thein Naing <theinnaing0908@gmail.com>
2023-05-19 17:28:00 +0800

    create controllers folder in server and create users.js controller file, destructure all routes from users.js in routes to controller by creating const getUsers/createUser e.t.c , so called split code and test in postman and it works

--- routes/users.js
+++ routes/users.js
@@ -1,73 +1,26 @@
 import express from 'express';
-import { v4 as uuidv4 } from 'uuid';
-// uuidv4(); // ⇨ '9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d'
+import { getUsers, getUser, createUser, deleteUser, updateUser } from '../controllers/users.js';
 
 const router = express.Router();
 
 
-let users =[
-  // {
-  //   firstName: "John",
-  //   lastName: "Wick",
-  //   age: "58"
-  //       },
-
-  // {
-  //   firstName: "Hellen",
-  //   lastName: "Wick",
-  //   age: "38"
-  //             }
-
-
-]
+let users =[]
 console.log(users);
 // all routes here start with users so we do not need to add users in /.
-router.get('/', (req, res)=> {
-  res.send(users)
-})
+router.get('/', getUser);
 
-router.post('/', (req, res)=> {
-  const user = req.body;
-
+router.post('/', createUser);
   // ⇨ '9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d
   // const userWithId = {...user, id: uuidv4() });
 
-  users.push({...user, id: uuidv4() });
-
-  res.send(`User with the name ${ user.firstName } added to the DB!`)
-});
-
-router.get('/:id', (req, res) => {
+ router.get('/:id', getUser);
   // console.log(req.params);
   // const id = req.params.id
-  const { id } = req.params;
-
-  const foundUser = users.find((user) => user.id === id);
-
   // res.send(req.params);
-  res.send(foundUser);
-});
+
+router.delete('/:id', deleteUser);
 
 
-router.delete('/:id', (req, res)=> {
-  const { id } = req.params;
-
-  const users = users.filter((user) => user.id !== id);
-  res.send(`User with the id ${id} deleted from DB.`);
-
-})
-
-
-router.patch('/:id', (req, res)=> {
-  const { id } = req.params;
-  const { firstName, lastName, age } = req.body;
-
-  const userUpdated = users.find((user) => user.id === id);
-  if (firstName) userUpdated.firstName = firstName;
-  if (lastName) userUpdated.lastName = lastName;
-  if (age) userUpdated.age = age;
-    res.send(`User with the id ${id} has been updated to DB.`);
-
-})
+router.patch('/:id', updateUser);
 
 export default router;
