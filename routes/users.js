2a03c274e491cc7cc45eebcf2bc32192218840b1
Thein Naing <theinnaing0908@gmail.com>
2023-05-19 14:29:45 +0800

    updateuser.push(user) function and test with postman by adding user objects in postman and send request and check in browser and we can see newly created user id

--- routes/users.js
+++ routes/users.js
@@ -1,22 +1,22 @@
 import express from 'express';
 import { v4 as uuidv4 } from 'uuid';
-uuidv4(); // ⇨ '9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d'
+// uuidv4(); // ⇨ '9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d'
 
 const router = express.Router();
 
 
 const users =[
-  {
-    firstName: "John",
-    lastName: "Wick",
-    age: "58"
-        },
+  // {
+  //   firstName: "John",
+  //   lastName: "Wick",
+  //   age: "58"
+  //       },
 
-  {
-    firstName: "Hellen",
-    lastName: "Wick",
-    age: "38"
-              }
+  // {
+  //   firstName: "Hellen",
+  //   lastName: "Wick",
+  //   age: "38"
+  //             }
 
 
 ]
@@ -31,12 +31,12 @@ router.post('/', (req, res)=> {
 
   // ⇨ '9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d
 
-  const userWithId = {...user, id: uuidv4( )}
+  // const userWithId = {...user, id: uuidv4() });
 
-  users.push(user)
+  users.push({...user, id: uuidv4() });
 
   res.send(`User with the name ${ user.firstName } added to the DB!`)
-})
+});
 
 
 
